e5f213ef33c12acd861ebb0655a0bf80e24b650a
mrmuhammadrifki <immuhammadrifki@gmail.com>
2023-07-31 11:14:24 +0700

    fix

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -4,27 +4,27 @@ const contentDetail = document.querySelector('.content-detail');
 const contentChat = document.querySelector('.content-chat');
 
 menuChat.addEventListener('click', function () {
-  menuDetail.classList.remove('active');
+  menuDetail.classList.remove('active-menu');
   menuDetail.querySelector('h6').classList.remove('fw-bold');
   menuDetail.querySelector('h6').classList.remove('active-color');
   menuDetail.querySelector('h6').classList.add('unactive-color');
 
   contentDetail.classList.add('d-none');
 
-  menuChat.classList.add('active');
+  menuChat.classList.add('active-menu');
   menuChat.querySelector('h6').classList.add('fw-bold');
   menuChat.querySelector('h6').classList.remove('unactive-color');
   menuChat.querySelector('h6').classList.add('active-color');
   contentChat.classList.remove('d-none');
 });
 menuDetail.addEventListener('click', function () {
-  menuChat.classList.remove('active');
+  menuChat.classList.remove('active-menu');
   menuChat.querySelector('h6').classList.remove('fw-bold');
   menuChat.querySelector('h6').classList.remove('active-color');
   menuChat.querySelector('h6').classList.add('unactive-color');
   contentChat.classList.add('d-none');
 
-  menuDetail.classList.add('active');
+  menuDetail.classList.add('active-menu');
   menuDetail.querySelector('h6').classList.add('fw-bold');
   menuDetail.querySelector('h6').classList.remove('unactive-color');
   menuDetail.querySelector('h6').classList.add('active-color');
